26691aa2621dfbd53ea432f2cb23a5f1948152ab
Alireza Akbari <a.r.akbari69@gmail.com>
2025-03-18 17:28:26 +0100

    Final

--- Readme.md
+++ Readme.md
@@ -1,50 +1,67 @@
 ## To Embed 5 messages into 5 images
 The initial step is to generate at least 5 copies of Test OutGuess 01 with different embedded messages
 
-`java -jar openstego.jar embed -mf ./assets/msg1.txt -cf ./assets/img1.jpg -sf ./assets/output/simg1.png`
-
-`java -jar openstego.jar embed -mf ./assets/msg2.txt -cf ./assets/img2.jpg -sf ./assets/output/simg2.png`
-
-`java -jar openstego.jar embed -mf ./assets/msg3.txt -cf ./assets/img3.jpg -sf ./assets/output/simg3.png`
-
-`java -jar openstego.jar embed -mf ./assets/msg4.txt -cf ./assets/img4.jpg -sf ./assets/output/simg4.png`
-
-`java -jar openstego.jar embed -mf ./assets/msg5.txt -cf ./assets/img5.jpg -sf ./assets/output/simg5.png`
+`steghide embed -p "" -ef ./assets/msg1.txt -cf ./assets/img1.jpg -sf ./assets/output/simg1.jpg`
+`steghide embed -p "" -ef ./assets/msg2.txt -cf ./assets/img2.jpg -sf ./assets/output/simg2.jpg`
+`steghide embed -p "" -ef ./assets/msg3.txt -cf ./assets/img3.jpg -sf ./assets/output/simg3.jpg`
+`steghide embed -p "" -ef ./assets/msg4.txt -cf ./assets/img4.jpg -sf ./assets/output/simg4.jpg`
+`steghide embed -p "" -ef ./assets/msg5.txt -cf ./assets/img5.jpg -sf ./assets/output/simg5.jpg`
 
 **now run the following robustness tests:**
 
 ## 1.Check if we can detect embedded message after rotation transformation
-run this command to see the result:
-`java -jar openstego.jar extract  -sf ./assets/transformation/rotation/rotated_simg1.png -xf ./assets/transformation/rotation/msg1.txt`
+Run this command to see the result:
+`steghide extract -p ""  -sf ./assets/transformation/rotation/rotated_simg1.jpg -xf ./assets/transformation/rotation/msg1.txt`
+`steghide extract -p ""  -sf ./assets/transformation/rotation/rotated_simg5.jpg -xf ./assets/transformation/rotation/msg5.txt`
 
-**Result is**: Embedded data is corrupt OR invalid password has been provided OR no algorithm found which can handle the given stego file
+**Result is**: Could not extract any data
 
-to check a normal extraction:
-`java -jar openstego.jar extract  -sf ./assets/output/simg1.png -xf ./assets/output/msg1.txt`
 
 ## 2. Check if we can detect embedded message after crop
-run this command to see the result:
-`java -jar openstego.jar extract  -sf ./assets/transformation/crop/cropped_simg1.png -xf ./assets/transformation/crop/msg1.txt`
+Run this command to see the result:
+`steghide extract -p ""  -sf ./assets/transformation/crop/cropped_simg1.jpg -xf ./assets/transformation/crop/msg1.txt`
+`steghide extract -p ""  -sf ./assets/transformation/crop/cropped_simg5.jpg -xf ./assets/transformation/crop/msg5.txt`
 
-**Result is**:Embedded data is corrupt OR invalid password has been provided OR no algorithm found which can handle the given stego file
+**Result is**: Could not extract any data
 
 ## 3. Check if we can detect embedded message after Jpeg comperssions
-run this command to see the result:
-`java -jar openstego.jar extract  -sf ./assets/transformation/comperssion/comp_simg1.jpeg -xf ./assets/transformation/comperssion/msg1.txt`
+Run this command to see the result:
+`steghide extract -p ""  -sf ./assets/transformation/comperssion/comp_simg1.jpg -xf ./assets/transformation/comperssion/msg1.txt`
+`steghide extract -p ""  -sf ./assets/transformation/comperssion/comp_simg2.jpg -xf ./assets/transformation/comperssion/msg2.txt`
 
-**Result is**:???? 
+**Result is**: Could not extract any data
+
+
+
+## 4. Check if we can detect embedded message after different MatLab Filters
+Run this command to see the result:
+`steghide extract -p ""  -sf ./assets/transformation/others/noise_reduced.jpg -xf ./assets/transformation/comperssion/msg1.txt`
+
+`steghide extract -p ""  -sf ./assets/transformation/others/gamma_corrected.jpg -xf ./assets/transformation/comperssion/msg2.txt`
+
+**Result is**: Could not extract any data
 
 <br><br><br>
 ---
-#### Sample Embed 
-`java -jar openstego.jar embed -mf ./assets/text.txt -cf ./assets/img.jpg -sf ./assets/export/output.png`
-`-mf`: Path to the message file (text file to hide)
-`-cf`: Path to the cover file
-`-sf`: Patht to the stego file
+### Sample Embed 
 
-#### Sample Extract 
-`java -jar openstego.jar extract  -sf ./assets/export/output.png -xf ./assets/export/txt/embeded.txt`
+`steghide embed -cf ./assets/img1.jpg -ef ./assets/msg1.txt -sf ./assets/output/stego.jpg -p ""`
+`-ef`: Path to the message file (text file to hide)
+`-cf`: Path to the cover file
+`-sf`: Path to the stego file
+
+### Sample Extract 
+`steghide extract  -sf ./assets/output/simg1.jpg -xf ./assets/output/msg1.txt -p ""`
+`-sf`: Path to the stego file
 `-xf`: Path where the extracted text will be saved (text file)
 
+## Requirement
+### For mac users
+1. Install Macport <a href="https://www.macports.org/install.php">https://www.macports.org/install.php</a>
+2. Install Steghide `sudo port install steghide`
+### For windows users
+Check offical repository of Steghide for windows users <a href="https://github.com/StegHigh/steghide?tab=readme-ov-file">https://github.com/StegHigh/steghide?tab=readme-ov-file</a>
+
 ## Reference 
-online documentation at [openstego.com](https://www.openstego.com/cmdline.html)
+Online documentation at [openstego.com](https://www.openstego.com/cmdline.html)
+
--- code.m
+++ code.m
@@ -5,16 +5,16 @@ asset.trans = "./assets/transformation/";
 
 
 % Read all stego images
-images = {imread(asset.output  + "simg1.png"),
-          imread(asset.output  + "simg2.png"),
-          imread(asset.output  + "simg3.png"),
-          imread(asset.output  + "simg4.png"),
-          imread(asset.output  + "simg5.png"),
+images = {imread(asset.output  + "simg1.jpg"),
+          imread(asset.output  + "simg2.jpg"),
+          imread(asset.output  + "simg3.jpg"),
+          imread(asset.output  + "simg4.jpg"),
+          imread(asset.output  + "simg5.jpg"),
           };
 
 % simple conversion
-img = imread(asset.output  + "simg1.png");
-imwrite(img, asset.output  + "simg1.jpeg");
+% img = imread(asset.output  + "simg1.png");
+% imwrite(img, asset.output  + "simg1.jpeg");
 
 % #################
 % Task 1: Rotation 
@@ -26,7 +26,7 @@ imwrite(img, asset.output  + "simg1.jpeg");
 % for i= 1:length(rotation)
 %     rotated_img = imrotate(images{i}, rotation(i));
 %     rotated_imgs{i} = rotated_img;
-%     imwrite(rotated_img, asset.trans + "/rotation/rotated_simg"+i+".png");
+%     imwrite(rotated_img, asset.trans + "/rotation/rotated_simg"+i+".jpg");
 % end
 % 
 % % Now check again after transformation, is it possible to check the
@@ -50,7 +50,7 @@ imwrite(img, asset.output  + "simg1.jpeg");
 %     new_height = round(height * (1-crop(i)));
 %     cropped_img = images{i}(1:new_height,:,:);
 %     cropped_images{i} = cropped_img; 
-%     imwrite(cropped_img, asset.trans + "/crop/cropped_simg"+i+".png");
+%     imwrite(cropped_img, asset.trans + "/crop/cropped_simg"+i+".jpg");
 % end
 % 
 % figure;
@@ -64,17 +64,102 @@ imwrite(img, asset.output  + "simg1.jpeg");
 % Task 3: Jpeg Comperssions
 % #########################
 
-comperssion = [100, 75, 50, 20, 10];
-comp_images = {}
-for i=1:length(images)
-    filename =  sprintf(asset.trans + "/comperssion/comp_simg" + i + ".jpeg");
-    imwrite(images{i},filename , 'Quality', comperssion(i));
-    comp_images{i} = imread(filename);
-end
+% comperssion = [100, 75, 50, 20, 10];
+% comp_images = {}
+% for i=1:length(images)
+%     filename =  sprintf(asset.trans + "/comperssion/comp_simg" + i + ".jpg");
+%     imwrite(images{i},filename , 'Quality', comperssion(i));
+%     comp_images{i} = imread(filename);
+% end
+% 
+% figure;
+% for i = 1:length(images)
+%     subplot(2, 3, i);
+%     imshow(comp_images{i});
+%     title(sprintf("%d.Comperssion: %d%%", i, comperssion(i)));
+% end
+
+
+% #########################
+% Task 4: Image Filters
+% #########################
+watermarked_img = imread(asset.output  + "simg1.jpg");
 
 figure;
-for i = 1:length(images)
-    subplot(2, 3, i);
-    imshow(comp_images{i});
-    title(sprintf("%d.Comperssion: %d%%", i, comperssion(i)));
-end
+subplot(3, 3, 1);
+imshow(watermarked_img);
+title("Original Watermarked Image");
+
+% Noise Reduction (Gaussian Filter)
+% 0.5 is mild smoothing 
+% 2 is strong blure (more noise removal, but image details are lost)
+noise_reduced = imgaussfilt(watermarked_img, 1); 
+subplot(3, 3, 2);
+imshow(noise_reduced);
+imwrite(noise_reduced, asset.trans + "/others/noise_reduced.jpg");
+title("Noise Reduction (Gaussian)");
+
+% Sharpening
+% 1 is medium sharpening 
+% 1.5 or 2 is more sharpening ( can introduce noise)
+sharpened = imsharpen(watermarked_img, 'Amount', 1);
+subplot(3, 3, 3);
+imshow(sharpened);
+imwrite(sharpened, asset.trans + "/others/sharpened.jpg");
+title("Sharpened Image");
+
+% Contrast Adjustment
+% stretchlim(img) => Automatically detects the optimal contrast range.
+contrast_adjusted = imadjust(watermarked_img, stretchlim(watermarked_img), []);
+subplot(3, 3, 4);
+imshow(contrast_adjusted);
+imwrite(contrast_adjusted, asset.trans + "/others/contrast_adjusted.jpg");
+title("Contrast Adjusted");
+
+% Gamma Correction
+% gamma < 1 like 0.5 Brighten the image
+% gamma > 1 like 2 Darken the image
+gamma_corrected = imadjust(watermarked_img, [], [], 0.5); % Gamma < 1 brightens the image
+subplot(3, 3, 5);
+imshow(gamma_corrected);
+imwrite(gamma_corrected, asset.trans + "/others/gamma_corrected.jpg");
+title("Gamma Correction (0.5)");
+
+
+% Color Depth Reduction (Reduce to 32 Levels)
+% 8-bit depth (256 colors) → img / 8 * 8 reduces to 32 levels.
+color_depth_reduced = uint8(floor(double(watermarked_img) / 8) * 8);
+subplot(3, 3, 6);
+imshow(color_depth_reduced);
+imwrite(color_depth_reduced, asset.trans + "/others/color_depth_reduced.jpg");
+title("Reduced Color Depth");
+
+% Add Gaussian Noise
+% mean = 0 => No shift in brightness.
+% variance = 0.01 => Controls noise strength
+noisy_img = imnoise(watermarked_img, 'gaussian', 0, 0.01); % Add Gaussian noise
+subplot(3, 3, 7);
+imshow(noisy_img);
+imwrite(noisy_img, asset.trans + "/others/noisy_img.jpg");
+title("Gaussian Noise");
+
+% Edge Detection (Sobel edge detection filter)
+% 'canny' -> More advanced edge detection.
+% 'prewitt' -> Similar to Sobel but less sensitive.
+gray_img = rgb2gray(watermarked_img);
+edges = edge(gray_img, 'sobel');
+subplot(3, 3, 8);
+imshow(edges);
+imwrite(gray_img, asset.trans + "/others/gray_img_sobel.jpg");
+title("Edge Detection (Sobel)");
+
+
+%Histogram Equalization (Enhance Contrast)
+% histeq(img) => Applies histogram equalization, enhancing contrast.
+% Works best on grayscale images.
+equalized = histeq(rgb2gray(watermarked_img));
+subplot(3, 3, 9);
+imshow(equalized);
+imwrite(equalized, asset.trans + "/others/equalized.jpg");
+title("Histogram Equalization");
+
